f59367e9149cf63e01bba83bb05fe1bd71f368a4
crazycoding <contoh@crazycoding.com>
2019-08-18 18:41:09 +0700

    initial code projek

--- reconstruct.php
+++ reconstruct.php
@@ -1,5 +1,5 @@
 <center>
-    <img src="https://ojekpulo.000webhostapp.com/assetss/images/reborn.jpg" width="60%">
+    <img src="http://fundesk.xyz/assetss/assets/images/reborn.jpg" width="60%">
     <br>
     <h2>DITUTUP SEMENTARA</h2>
 </center>
