94ddaa63a523068bad122a6a286519a9b2c37bfc
crazycoding <contoh@crazycoding.com>
2019-08-18 11:40:32 +0700

    initial code projek

--- application/models/Api_ticket.php
+++ application/models/Api_ticket.php
@@ -78,7 +78,7 @@ class Api_ticket extends CI_Model{
 				<body style='margin: 10px;'>
 					<div style='width: 1000px; font-family: Helvetica, sans-serif; font-size: 13px; padding:10px; line-height:150%; border:#eaeaea solid 10px;'>
 						
-						<strong>Yang terhormat Bapak/Ibu/Saudara/i .$rowidadmin[0]->user_first_name.' '.$rowidadmin[0]->user_last_name. , pengajuan layanan anda sudah berhasil tersimpan. Berikut data pengajuan layanan anda :</strong><br>
+						<p>Yang terhormat Bapak/Ibu/Saudara/i ".$rowuser[0]->user_first_name." ".$rowuser[0]->user_last_name.", pengajuan layanan anda sudah berhasil tersimpan. Berikut data pengajuan layanan anda :</p><br>
 						<b>Nomor Tiket : </b>".$tiketnum."<br>
 						<b>Ringkasan Permasalah : </b>".$ps."<br>
 						<b>Detail Permasalahan : </b>".$pd."<br>
