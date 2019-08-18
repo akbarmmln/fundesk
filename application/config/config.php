2380f8be97c3892ff5477aeac5247f0174318828
crazycoding <contoh@crazycoding.com>
2019-08-18 08:47:41 +0700

    update config and db

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://fundesl.xyz/';
+$config['base_url'] = 'http://fundesk.xyz/';
 
 /*
 |--------------------------------------------------------------------------
